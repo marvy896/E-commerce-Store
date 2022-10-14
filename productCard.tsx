4c79879ab7225e153dd636b54ed03407f8814f03
marvy896 <onyex896@gmail.com>
2022-10-14 09:20:24 +0100

    The search and sort vfunctions is live!

--- productCard.tsx
+++ productCard.tsx
@@ -1,25 +1,69 @@
-import React from 'react'
-import Nav from './nav';
+import React, { FormEvent, useState } from "react";
+import Nav from "./nav";
 // import {Col, Row} from "react-bootstrap"
-import  storeItems from './src/items.json';
-import StoreItems from './src/components/StoreItems';
+import storeItems from "./src/items.json";
+import StoreItems from "./src/components/StoreItems";
+import { StoreItemProps } from "./src/components/StoreItems";
 
 
 export default function ProductCard() {
+  let [sortArray, setSortedArray] = useState<StoreItemProps[]>([]);
+  let [SortedText, setSortedText] = useState("");
+  let [maintext, setMainText] = useState("");
+  let [text, setText] = useState("");
+  let [textDisplay, setTextDisplay] = useState<StoreItemProps[]>([]);
+ 
+  //to sort prices
+  const SortPrd = (itemsArray ?:StoreItemProps[]) => {
+    itemsArray = itemsArray || storeItems
+    let sortPrd = itemsArray.sort((a, b) => a.price - b.price);
+    setSortedText(text);
+    console.log(sortPrd);
+    setSortedArray(sortPrd);
+    return sortPrd
+  };
+  const searchProducts = (e: FormEvent) => {
+    e.preventDefault()
+    let searchPrd = storeItems.filter(
+      (x) =>
+        x.nameProduct.toUpperCase().includes(text.toUpperCase()) ||
+        x.price <= parseInt(text)
+    );
+   setTextDisplay(SortPrd(searchPrd));
+    setMainText(text);
+  };
+
   return (
     <div>
       <Nav />
+      <div className="prdouctCard">
         <h2>Store</h2>
-        <div className="store">
-        {storeItems.map((item: JSX.IntrinsicAttributes & { id: number; nameProduct: string; price: number; imgUrl: string; }) => (
-          <div key ={item.id}>
-            <StoreItems {...item}/>
+        <form onSubmit={searchProducts}><input
+          className="searchInput"
+          placeholder="Search something here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+        <button className="searchbtn" onClick={searchProducts}>
+          Search
+        </button></form>
+      </div>
+      <div className="store">
+        {(maintext == "" ? storeItems : textDisplay).map(
+          (
+            item: JSX.IntrinsicAttributes & {
+              id: number;
+              nameProduct: string;
+              price: number;
+              imgUrl: string;
+            }
+          ) => (
+            <div key={item.id}>
+              <StoreItems {...item} />
             </div>
-        ))}
-        </div>
+          )
+        )}
+      </div>
     </div>
-    
-  )
+  );
 }
-
-
--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import useCart from '../hooks/hooks';
 
 
-type StoreItemProps = {
+export type StoreItemProps = {
   id: number;
   nameProduct: string;
   price: number;
